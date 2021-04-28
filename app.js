238ae24779b08f170f49c61638578ec4c79f72d9
ThibaudS <thibaud.devpro@gmail.com>
2021-04-28 11:18:08 +0200

    Added easy Middleware

--- app.js
+++ app.js
@@ -2,8 +2,24 @@ const express = require('express')
 
 const app = express()
 
-app.use((req, res) => {
-    res.json({ message: "votre requête a bien été reçu !" })
+app.use((req, res, next) => {
+    console.log('Requête reçue !')
+    next()
 })
+
+app.use((req, res, next) => {
+    res.status(201)
+    next()
+})
+
+app.use((req, res, next) => {
+    res.json({ message: "votre requête a bien été reçu !" })
+    next()
+})
+
+app.use((req, res) => {
+    console.log('Réponse envoyée avec succès !')
+})
+
 
 module.exports = app
