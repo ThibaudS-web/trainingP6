20974512a08ee98e105e206c2e892ae7dac456bc
ThibaudS <thibaud.devpro@gmail.com>
2021-04-28 14:51:13 +0200

    Recording Things models in Data-Base

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
+const Thing = require('./models/Thing')
+
 const app = express()
 
 mongoose.connect('mongodb+srv://ThibS:14y3X3DDA3FPSrFA@cluster0.ffla6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
@@ -21,33 +23,18 @@ app.use((req, res, next) => {
 app.use(bodyParser.json())
 
 app.post('/api/stuff', (req, res, next) => {
-    console.log(req.body)
-    res.status(201).json({
-        message: 'Objet crée !'
-    })
-})
+    delete req.body._id;
+    const thing = new Thing({
+      ...req.body
+    });
+    thing.save()
+      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+      .catch(error => res.status(400).json({ error }));
+  });
 
 app.use('/api/stuff', (req, res, next) => {
-    const stuff = [
-      {
-        _id: 'oeihfzeoi',
-        title: 'Mon premier objet',
-        description: 'Les infos de mon premier objet',
-        imageUrl: 'https://cdn.motor1.com/images/mgl/Ee7MM/s1/bugatti-la-voiture-noire-roadster-rendering-lead-image.jpg',
-        price: 4900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'oeihfzeomoihi',
-        title: 'Mon deuxième objet',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-    ];
-    res.status(200).json(stuff);
-  });
+
+});
 
 
 module.exports = app
