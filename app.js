9bb65f0d77bb6ff598160853d6d4b30f2850671d
ThibaudS <thibaud.devpro@gmail.com>
2021-05-12 10:35:24 +0200

    Added route and controllers

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
-const Thing = require('./models/Thing')
-
-const app = express()
+const stuffRoutes = require('./route/stuff')
 
 mongoose.connect('mongodb+srv://ThibS:14y3X3DDA3FPSrFA@cluster0.ffla6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     {   useNewUrlParser: true,
@@ -12,6 +10,7 @@ mongoose.connect('mongodb+srv://ThibS:14y3X3DDA3FPSrFA@cluster0.ffla6.mongodb.ne
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'))
 
+const app = express()
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*')
@@ -22,39 +21,7 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json())
 
-app.post('/api/stuff', (req, res, next) => {
-    delete req.body._id;
-    const thing = new Thing({
-      ...req.body
-    });
-    thing.save()
-      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-      .catch(error => res.status(400).json({ error }))
-});
-
-app.put('/api/stuff/:id', (req, res, next) => {
-    Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() =>  res.status(200).json({ message: 'Objet modifié !' }))
-    .catch(error => res.status(400).json({ error }))
-})
-
-app.delete('/api/stuff/:id', (req, res, next) => {
-    Thing.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-    .catch(error => res.status(400).json({ error }))
-})
-
-app.get('/api/stuff/:id', (req, res, next) => {
-Thing.findOne({ _id: req.params.id })
-    .then(thing => res.status(200).json(thing))
-    .catch(error => res.status(404).json({ error }))
-});
-
-app.get('/api/stuff', (req, res, next) => {
-    Thing.find()
-    .then(things => res.status(200).json(things))
-    .catch(error => res.status(400).json({ error }))
-});
+app.use('/api/stuff', stuffRoutes)
 
 
 module.exports = app
